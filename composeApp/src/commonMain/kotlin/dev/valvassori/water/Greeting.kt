ec426f005ecb9eac35693f3fa14a5ea0bb10d269
Gustavo Fao Valvassori <faogustavo@gmail.com>
2024-10-24 21:13:22 -0300

    removed template code

--- composeApp/src/commonMain/kotlin/dev/valvassori/water/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.valvassori.water
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/dev/valvassori/water/Platform.kt
+++
@@ -1,7 +0,0 @@
-package dev.valvassori.water
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
